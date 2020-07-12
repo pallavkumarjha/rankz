875d027a8ba153587ca11751d569a93fdbc78346
Pallav <pallav.jha@selfinindia.com>
2020-07-12 20:22:54 +0530

    Added static page for assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rankz';
+  sideMenuList: MenuItem[] = [
+    {
+      hasSubMenu: false,
+      icon: 'assets/images/dashboard.svg',
+      name: 'Dashboard',
+    },{
+      hasSubMenu: true,
+      icon: 'assets/images/site_exp.svg',
+      name: 'Site Explorer',
+    },{
+      hasSubMenu: true,
+      icon: 'assets/images/key_exp.svg',
+      name: 'Keyword Explorer',
+    },{
+      hasSubMenu: false,
+      icon: 'assets/images/rank_trc.svg',
+      name: 'Rank Tracker',
+    },{
+      hasSubMenu: true,
+      icon: 'assets/images/dist_con.svg',
+      name: 'Distribute Content',
+    },{
+      hasSubMenu: true,
+      icon: 'assets/images/ppc_exp.svg',
+      name: 'PPC Explorer',
+    },{
+      hasSubMenu: true,
+      icon: 'assets/images/others.svg',
+      name: 'More Tools',
+    },{
+      hasSubMenu: false,
+      icon: 'assets/images/integ.svg',
+      name: 'Integrations',
+    }
+  ]
+}
+
+export interface MenuItem{
+  name: string;
+  hasSubMenu: boolean;
+  icon: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
